e6d27b0d2cf9c561ab0899e9b7e82c31b59a6d2f
Christine Cua <cloudycua@gmail.com>
2019-02-14 00:20:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,4 @@
+// Create a constructor function for each of the following:
+// -Scooter with year, color, and model properties
+// -Driver with name, age, and experience properties
+// -PickupLocation with address and city properties
