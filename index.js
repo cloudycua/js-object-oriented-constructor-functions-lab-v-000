34d9dbdc35c1378fb3ee43ef7c1b56851427eab6
Christine Cua <cloudycua@gmail.com>
2019-02-14 00:20:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Create a constructor function for each of the following:
 // -Scooter with year, color, and model properties
+//  samson = new Scooter(2015, "red", "swoosh");
 // -Driver with name, age, and experience properties
 // -PickupLocation with address and city properties
